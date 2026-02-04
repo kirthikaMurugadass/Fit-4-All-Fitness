08f23840e092c2ca8e2a89c3aefc718c72ef56c3
kirthikaMurugadass <kirthika@maxappzrh.com>
2026-02-04 14:32:54 +0530

    Writeclient changes

--- sanity/lib/writeClient.ts
+++ sanity/lib/writeClient.ts
@@ -3,7 +3,7 @@ import { createClient } from "next-sanity"
 export const writeClient = createClient({
   projectId: "qjda8zjr",
   dataset: "production",
-  apiVersion: "2026-02-02",
+  apiVersion: "process.env.NEXT_PUBLIC_SANITY_API_VERSION",
   useCdn: false, // IMPORTANT
   token: process.env.SANITY_WRITE_TOKEN, // 🔐
 })
